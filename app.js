ea9b989b20c36d22a6d0b6219b9774737898aff4
Numrahmubeen <numrahmubeen@gmail.com>
2022-01-17 13:45:50 +0500

    6 API endpoint added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Todo = require("./models/Todo");
 
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 //enable form post variables : a config change
 app.use(express.urlencoded({extended:true}));
@@ -32,6 +32,106 @@ app.get('/',(req,res)=>{
           console.error(error);
   });
 }) 
+//get pending todos
+app.get('/pending',(req,res)=>{
+
+  Todo.findAll({
+    where: {
+      todo_state: "P",
+    }
+  }).then((err,todos) =>{
+    if(!err){
+      res.send(result.rows);
+    }
+    else{
+      res.send(err);
+    }
+  }).catch(function(error) { 
+        console.error(error);
+});
+}) 
+//get Complete todos
+app.get('/complete',(req,res)=>{
+
+  Todo.findAll({
+    where: {
+      todo_state: "C",
+    }
+  }).then((err,todos) =>{
+    if(!err){
+      res.send(result.rows);
+    }
+    else{
+      res.send(err);
+    }
+  }).catch(function(error) { 
+        console.error(error);
+      });
+});
+//get Todo by Id
+app.get('/todos/:id',(req,res)=>{
+
+  Todo.findAll({
+    where: {
+      todo_id: req.params.id,
+    }
+  }).then((err,todos) =>{
+    if(!err){
+      res.send(result.rows);
+    }
+    else{
+      res.send(err);
+    }
+  }).catch(function(error) { 
+        console.error(error);
+      });
+});
+
+//add todo post route
+app.post("/saveTodo",(req,res)=>{
+
+  let todo = req.body;
+
+  Todo.create({
+    todo_name: todo.todo_name, 
+    todo_title: todo.todo_title,
+    todo_desc: todo.todo_desc,
+    todo_state:"P"}
+    )
+    .then((response,err)=>{
+      if(!err){
+          res.send('Insertion was successful')
+      }
+      else{ 
+        res.send(err)
+        console.log(err.message) 
+      }
+    })
+})
+//edit todo state
+app.post("/edit/:todo_id",(req,res)=>{
+
+  let state = req.body.state;
+  // if(state == "C"){
+  //   state = "P";
+  // }else{
+  //   state = "C";
+  // }
+  Todo.update({ todo_state: state }, {
+      where: {
+          todo_id: req.params.todo_id
+      }
+    })
+    .then((response,err)=>{
+      if(!err){
+        res.send("Todo state updated sucessfully!")
+      }
+      else{ 
+        res.send(err)
+        console.log(err.message) 
+      }
+    });
+});
 
 app.listen(port, function (error) {
     if (error)
